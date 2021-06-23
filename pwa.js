87868c1b127233c92ad8eddc14bc82afb7c1cf55
fetchproxy <85281752+fetchproxy@users.noreply.github.com>
2021-06-23 15:53:44 +0800

    Update pwa.js

--- pwa.js
+++ pwa.js
@@ -2,9 +2,15 @@ if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('sw.js');
 };
 
-let deferredPrompt;
+var deferredPrompt;
 window.addEventListener("beforeinstallprompt", (e) => {
     e.preventDefault();
     deferredPrompt = e;
     console.log("'beforeinstallprompt' event was fired.")
 });
+
+async function installapp() {
+    deferredPrompt.prompt();
+    const { outcome } = await deferredPrompt.userChoice;
+    console.log(`user response to the install prompt:${outcome}`);
+};
