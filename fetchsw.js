10aa623449317635443337043ca60d9acfbdf76f
fetchproxy <85281752+fetchproxy@users.noreply.github.com>
2021-06-29 17:29:37 +0800

    Delete fetchsw.js

--- fetchsw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener("fetch", function (event) {
-    let oldURL = event.request.url;
-    newURL = new URL(oldURL);
-    if (newURL.host.toLowerCase().includes("google.com")) {
-        return fetch("https://search.getfetch.workers.dev/?url=" + oldURL, event.request);
-    } else {
-        return fetch(event.request);
-    }
-})
