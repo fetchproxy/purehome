1756a683366d1f6f6c13bdb414a2eca0ab915c2c
fetchproxy <85281752+fetchproxy@users.noreply.github.com>
2022-03-13 14:39:30 +0800

    Update sites.js

--- sites.js
+++ sites.js
@@ -1,6 +1,6 @@
 const sites = [{
     name: "AV淘宝",
-    url: "http://www.avtb2170.com",
+    url: "http://www.avtb2172.com",
     pub: "http://www.avtbdizhi.org/",
     email: "",
     latest: "recent",
